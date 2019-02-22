73ba2014b61fa3e7bfe6d136835f878529e41485
jhonata.galante@gmail.com <jhonata.galante@gmail.com>
2019-02-22 11:47:55 -0300

    modificação resolução das imagens

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlmini = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 var scssFiles = './app/scss/*.scss';
 var htmlFiles = './app/*.html';
 var jsFiles = './app/js/*.js';
+var imagesFile = './app/img'
 
 var cssDistDest = './dist/css';
 var htmlDistDest = './dist';
 var jsDistDest = './dist/js';
+var imagesDisDest = './dist/img';
 
 
 // Options for production
@@ -22,6 +25,12 @@ function sassprod() {
         .pipe(sass(sassProdOptions).on('error', sass.logError))
         .pipe(gulp.dest(cssDistDest));
 };
+
+function minifyimage(){
+    return gulp.src(imagesFile)
+      .pipe(imagemin())
+      .pipe(gulp.dest(imagesDisDest))
+}
 
 function htmlminify(){
     return gulp.src(htmlFiles)
@@ -36,15 +45,19 @@ function jsminify(){
 }
 
 function watchSCSS() {
-    gulp.watch(scssFiles, gulp.parallel(sassprod));
+    gulp.watch(scssFiles, sassprod);
 }
 
 function watchHTML(){
-    gulp.watch(htmlFiles, gulp.parallel(htmlminify));
+    gulp.watch(htmlFiles, htmlminify);
 }
 
 function watchScript(){
-    gulp.watch(jsFiles, gulp.parallel(jsminify));
+    gulp.watch(jsFiles, jsminify);
 }
 
-gulp.task('default', gulp.parallel(sassprod, watchSCSS, watchHTML, watchScript, htmlminify, jsminify));
+function watchImages(){
+    gulp.watch(imagesFile, minifyimage);
+}
+
+gulp.task('default', gulp.parallel(sassprod, watchSCSS, watchHTML,watchImages, watchScript,minifyimage, htmlminify, jsminify));
