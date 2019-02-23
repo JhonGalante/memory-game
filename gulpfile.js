117925d3fd9f0b11c48e414b612ccab3c6fe4e94
JhonGalante <jhonata.galante@gmail.com>
2019-02-22 22:13:55 -0300

    fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlmini = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 
 var scssFiles = './app/scss/*.scss';
 var htmlFiles = './app/*.html';
 var jsFiles = './app/js/*.js';
-var imagesFile = './app/img'
 
 var cssDistDest = './dist/css';
 var htmlDistDest = './dist';
 var jsDistDest = './dist/js';
-var imagesDisDest = './dist/img';
 
 
 // Options for production
@@ -25,12 +22,6 @@ function sassprod() {
         .pipe(sass(sassProdOptions).on('error', sass.logError))
         .pipe(gulp.dest(cssDistDest));
 };
-
-function minifyimage(){
-    return gulp.src(imagesFile)
-      .pipe(imagemin())
-      .pipe(gulp.dest(imagesDisDest))
-}
 
 function htmlminify(){
     return gulp.src(htmlFiles)
@@ -56,8 +47,5 @@ function watchScript(){
     gulp.watch(jsFiles, jsminify);
 }
 
-function watchImages(){
-    gulp.watch(imagesFile, minifyimage);
-}
 
-gulp.task('default', gulp.parallel(sassprod, watchSCSS, watchHTML,watchImages, watchScript,minifyimage, htmlminify, jsminify));
+gulp.task('default', gulp.parallel(sassprod, watchSCSS, watchHTML, watchScript,htmlminify, jsminify));
